c589fbfcf9b79cab80ab240a72034b15ce66133c
vorband <andre@vorbach.org>
2017-08-18 21:43:20 +0200

    fixed bind to host

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 [ ! -f /etc/ews/peba.cfg ] && echo "### PEBA: Config file /etc/ews/peba.cfg missing. Copy peba.cfg to /etc/ews/. Abort." && exit 1
 
-BIND=$(cat /etc/ews/peba.cfg|grep LISTENIP|cut -d "=" -f2|cut -d "\"" -f 2):$(cat /etc/ews/peba.cfg|grep LISTENPORT|cut -d "=" -f2)
+BIND=$(cat /etc/ews/peba.cfg|grep BINDHOST|cut -d "\"" -f2)
+
 
 pip install -r requirements.txt
 
