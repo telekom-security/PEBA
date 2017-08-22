c07b9246e2fea270304c19d7d3389f8c43382eaa
André Vorbach <andre@vorbach.org>
2017-08-22 09:44:56 +0200

    run as peba user, reworked ansible & deployment scripts

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i ./hosts site.yml
+if [ "$#" -ne 1 ]; then
+    echo "invoke: $0 <test|prod>"
+    exit 1
+fi
+
+case "$1" in
+    test)
+        echo "Deploying on Test-Server"
+        ansible-playbook -i ./hosts ewsbackend-test.yml
+      ;;
+    prod)
+        echo "Deploying on Prod-Server"
+        ansible-playbook -i ./hosts ewsbackend-prod.yml
+      ;;
+    *)
+       echo "invoke: $0 <test|prod>"
+      ;;
+esac
+exit 0
--- misc/test.sh
+++ misc/test.sh
@@ -1,78 +1,104 @@
 
 #!/bin/bash 
 
-BIND=127.0.0.1:9922
+# CONFIG:
+TEST=http://127.0.0.1:9922
+PROD=https://community.sicherheitstacho.eu:9443
 AUTH=request.xml
+
+
+
+if [ "$#" -ne 1 ]; then
+    echo "invoke: $0 <test|prod>"
+    exit 1
+fi
+
+case "$1" in
+    test)
+        BIND=$TEST
+        echo "Testing with Test-Server $BIND"
+      ;;
+    prod)
+        BIND=$PROD
+        echo "Testing with Prod-Server $BIND"
+      ;;
+    *)
+        BIND=$PROD
+      ;;
+esac
+
 
 echo "***** TESTING LOCAL WEBSERVICE" 
 
 echo "***** RETRIEVEALERTCYBER *****"
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./$AUTH http://$BIND/alert/retrieveAlertsCyber
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./$AUTH $BIND/alert/retrieveAlertsCyber
 echo "***** END RETRIEVEALERTSCYBER *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVEIPS *****"
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./$AUTH http://$BIND/alert/retrieveIPs
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./$AUTH $BIND/alert/retrieveIPs
 echo ""
 echo "***** END RETRIEVEIPS *****"
 echo ""
 sleep 3   
 
 echo "***** RETRIEVEALERTCOUNT XML  *****"
-curl http://$BIND/alert/retrieveAlertsCount?time=10
+curl $BIND/alert/retrieveAlertsCount?time=10
 echo "***** END RETRIEVEALERTCOUNT XML  *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVEALERTCOUNT JSON  *****"
-curl "http://$BIND/alert/retrieveAlertsCount?time=10&out=json"
+curl "$BIND/alert/retrieveAlertsCount?time=10&out=json"
 sleep 3
 echo "***** END RETRIEVEALERTCOUNT  JSON *****"
 echo ""
 sleep 3
 
 echo "***** HEARTBEAT  *****"
-curl http://$BIND/heartbeat
+curl $BIND/heartbeat
 echo "" 
 echo "***** END HEARTBEAT  *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVEALERTJSON  *****"
-curl http://$BIND/alert/retrieveAlertsJson
+curl $BIND/alert/retrieveAlertsJson
 echo "***** END RETRIEVEALERTJSON  *****"
 echo "" 
 sleep 3
 
 echo "***** RETRIEVEALERTSPERMONTH  *****"
-curl http://$BIND/alert/datasetAlertsPerMonth
+curl $BIND/alert/datasetAlertsPerMonth
 echo "***** END RETRIEVEALERTSPERMONTH  *****"
 echo ""
 sleep 3 
 
 echo "***** RETRIEVEALERTTYPESSPERMONTH  *****"
-curl http://$BIND/alert/datasetAlertTypesPerMonth
+curl $BIND/alert/datasetAlertTypesPerMonth
 echo "***** END RETRIEVEALERTYPESPERMONTH  *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVEALERTSTATS  *****"
-curl http://$BIND/alert/retrieveAlertStats
+curl $BIND/alert/retrieveAlertStats
 echo "***** END RETRIEVEALERTSTATS  *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVETOPCOUNTRIES  *****"
-curl http://$BIND/alert/topCountriesAttacks?offset=1&topx=4
+curl $BIND/alert/topCountriesAttacks?offset=1&topx=4
 echo "***** END RETRIEVETOPCOUNTRIES *****"
 echo ""
 sleep 3
 
 echo "***** RETRIEVELATLONG  *****"
-curl http://$BIND/alert/retrieveLatLonAttacks?offset=3&topx=4&direction=src
+curl $BIND/alert/retrieveLatLonAttacks?offset=3&topx=4&direction=src
 echo "***** END RETRIEVELATLONG *****"
 echo ""
+sleep 3
+
 
 echo "***** END TESTING LOCAL WEBSERVICE"
-
+exit 0
