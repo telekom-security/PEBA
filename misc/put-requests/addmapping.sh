9b3161053d314b3185c4dc288f0c8c22181857dd
vorband <andre@vorbach.org>
2017-09-12 14:14:45 +0200

    added ip mapping

--- misc/put-requests/addmapping.sh
+++ misc/put-requests/addmapping.sh
@@ -28,8 +28,6 @@ curl -XPUT 'localhost:9200/ews2017.1/_mapping/CVE?pretty' -H 'Content-Type: appl
 curl -XPUT 'localhost:9200/ews2017.1/_mapping/Alert?pretty' -H 'Content-Type: application/json' -d'
 {
   "properties": {
-
-
                     "createTime": {
                         "type": "date",
                         "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
@@ -46,6 +44,31 @@ curl -XPUT 'localhost:9200/ews2017.1/_mapping/Alert?pretty' -H 'Content-Type: ap
                     },
                     "clientDomain": {
                         "type": "boolean"
+                    }
+  }
+}
+'
+
+curl -XPUT 'localhost:9200/ews2017.1/_mapping/IP?pretty' -H 'Content-Type: application/json' -d'
+{
+  "properties": {
+                    "ip": {
+                        "type": "ip"
+                    },
+                    "longitude": {
+                        "type": "text"
+                    },
+                    "latitude": {
+                        "type": "text"
+                    },
+                    "asn": {
+                        "type": "text"
+                    },
+                    "country": {
+                        "type": "text"
+                    },
+                                        "countyname": {
+                        "type": "text"
                     }
   }
 }
