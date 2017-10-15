bd813dcc5cdaa2127f3874d744ecc7738a7d3f4b
schmalle <schmall.markus@gmail.com>
2017-10-15 08:54:52 +0200

    fixed url

--- misc/put-requests/getcyberalert.sh
+++ misc/put-requests/getcyberalert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./alarm.xml http://127.0.0.1:9922/retrieveAlertsCyber
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./alarm.xml http://127.0.0.1:9922/alert/retrieveAlertsCyber
 
