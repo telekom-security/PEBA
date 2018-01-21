0e993b3bde17188a8a2fa9ebc1888e089b6e9987
schmalle <schmall.markus@gmail.com>
2018-01-21 18:53:56 +0100

    bug fix

--- misc/tools/checkpackets.py
+++ misc/tools/checkpackets.py
@@ -22,7 +22,7 @@ print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
     data = hit["_source"]["data"].encode('utf-8')
     data = base64.b64decode(data)
-    print(data)
+   # print(data)
 
     m = hashlib.sha256()
     m.update(data.lower())
@@ -37,31 +37,37 @@ for hit in res['hits']['hits']:
         checksumssha256[hash] = counterForHash + 1
         duplicatessha256 += 1
 
-    if (b'Host:' in data):
+    data = str(data)
 
-        start = data.find(b"Host:") + 5
-        end = data.find(b"\"", start)
+    if ('Host:' in data):
 
-        print("Host found: ",start, end)
+        start = data.find("Host:") + 5
+        end = data.find('\\', start)
 
-        cleanedData = data[0:start] + data[end:len(data)]
+        print("Host found: ", start, end)
+
+        if end != -1:
 
 
-        m = hashlib.sha256()
-        m.update(cleanedData.lower())
+            cleanedData = data[0:start] + data[end:len(data)]
+            cleanedData = str(cleanedData)
+            print("Cleaned data: " + cleanedData)
 
-        hash = m.hexdigest()
 
-        counterForHash = checksumsfuzzy.get(hash, 0)
-        if (counterForHash == 0):
-            uniquesfuzzy += 1
-            checksumsfuzzy[hash] = 1
-        else:
-            checksumsfuzzy[hash] = counterForHash + 1
-            duplicatesfuzzy += 1
+            m = hashlib.sha256()
+            m.update(cleanedData.lower().encode('utf-8'))
 
-    else:
-        print("Host not found")
+            hash = m.hexdigest()
+
+            counterForHash = checksumsfuzzy.get(hash, 0)
+            if (counterForHash == 0):
+                uniquesfuzzy += 1
+                checksumsfuzzy[hash] = 1
+            else:
+                checksumsfuzzy[hash] = counterForHash + 1
+                duplicatesfuzzy += 1
+
+
 
 
 
