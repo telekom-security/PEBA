1aeabab46f513ae179604b43c349ed5b79ad76c2
vorband <andre@vorbach.org>
2018-01-29 13:32:11 +0100

    verbose output for detailed honeypot statistics

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -7,6 +7,7 @@
 import datetime, sys
 from elasticsearch import Elasticsearch, ElasticsearchException
 import ipaddress
+import argparse
 
 
 
@@ -61,8 +62,10 @@ def getNumberHoneypotsAndAlerts(timeframe, clientDomain, type):
     numHoneypots = 0
     numHoneypotsOld = 0 # pre 17.10
     internalDocker = 0
+    listoutput=""
 
     if clientDomain:
+        listoutput+="\n ------ detailed community honeypot statistics ------\n\n"
         try:
             # find all 17.10 T-Pots
             res = es.search(index=esindex, body={
@@ -114,11 +117,13 @@ def getNumberHoneypotsAndAlerts(timeframe, clientDomain, type):
               }
             })
             for i in range(len(res['aggregations']['hostnames']['buckets'])):
-                # print(res['aggregations']['hostnames']['buckets'][i]['key'] + str(res['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets']))
+                #print(res['aggregations']['hostnames']['buckets'][i]['key'] + str(res['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets']))
+                for j in res['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets']:
+                    listoutput+=("[" + res['aggregations']['hostnames']['buckets'][i]['key'] + "]" + "[" + j['key'] + "] : " + str(j['doc_count'])+ "\n")
                 numHoneypotDaemons+=len(res['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets'])
 
             numHoneypots=len(res['aggregations']['hostnames']['buckets'])
-            print("COMMUNTIY >= 17.10 --> " + str(numHoneypots) + " T-Pot installations with a total of " + str(numHoneypotDaemons) + " honeypot daemons, accounting for " +str(res['hits']['total'])+ " alerts.")
+            print("COMMUNITY >= 17.10 --> " + str(numHoneypots) + " T-Pot installations with a total of " + str(numHoneypotDaemons) + " honeypot daemons, accounting for " +str(res['hits']['total'])+ " alerts.")
 
 
             # Find older Honeypots via dest_ip
@@ -177,6 +182,9 @@ def getNumberHoneypotsAndAlerts(timeframe, clientDomain, type):
             })
 
             for i in range(len(res2['aggregations']['hostnames']['buckets'])):
+                for j in res2['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets']:
+                    listoutput+=("[" + res2['aggregations']['hostnames']['buckets'][i]['key'] + "]" + "[" + j['key'] + "] : " + str(j['doc_count']) + "\n")
+
                 if ipaddress.ip_address(res2['aggregations']['hostnames']['buckets'][i]['key']) in ipaddress.ip_network('172.16.0.0/12'):
                     #print("interne docker ip addresse : " + res2['aggregations']['hostnames']['buckets'][i]['key']+ " ---> " + str(res2['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets']))
                     internalDocker+=len(res2['aggregations']['hostnames']['buckets'][i]['peerIdents']['buckets'])
@@ -188,15 +196,21 @@ def getNumberHoneypotsAndAlerts(timeframe, clientDomain, type):
             numHoneypotsOld=len(res2['aggregations']['hostnames']['buckets'])
             print("COMMUNITY < 17.10 --> " + str(numHoneypotsOld) + " T-Pot installations with a total of " + str(numHoneypotDaemonsOld) + " honeypot daemons, accounting for " + str(res2['hits']['total'])+ " alerts -  including " + str(internalDocker) + " hosts with internal docker ip (might be counted only once)")
 
-
             # total sum
-            print("COMMUNTY TOTAL : " + str(numHoneypots+numHoneypotsOld) + " T-Pot installations, with a total of " + str(numHoneypotDaemons+numHoneypotDaemonsOld) + " honeypot daemons, accounting for " + str(res['hits']['total']+res2['hits']['total']) + " alerts." )
+            print("COMMUNITY TOTAL : " + str(numHoneypots+numHoneypotsOld) + " T-Pot installations, with a total of " + str(numHoneypotDaemons+numHoneypotDaemonsOld) + " honeypot daemons, accounting for " + str(res['hits']['total']+res2['hits']['total']) + " alerts." )
+
+            if args.verbose:
+                print(listoutput)
+
+
             return numHoneypots+numHoneypotsOld, numHoneypotDaemons+numHoneypotDaemonsOld, res['hits']['total']+res2['hits']['total']
 
 
         except ElasticsearchException as err:
             print('ElasticSearch error: %s' % err)
     else:
+        listoutput+="\n ------ detailed DTAG honeypot statistics ------\n\n"
+
         try:
             res = es.search(index=esindex, body={
                   "query": {
@@ -225,7 +239,14 @@ def getNumberHoneypotsAndAlerts(timeframe, clientDomain, type):
                   },
                   "size": 0
                 })
+
             print("DTAG TOTAL --> " + str(len(res['aggregations']['communityfilter']['hostnames']['buckets'])) + " honeypot daemons, accounting for " +  str(res['aggregations']['communityfilter']['doc_count']) + " alerts.")
+
+            for i in res['aggregations']['communityfilter']['hostnames']['buckets']:
+                listoutput+="[DTAG][" + i['key'] + "] : " + str(i['doc_count']) + "\n"
+
+            if args.verbose:
+                print(listoutput)
 
             return "unknown",len(res['aggregations']['communityfilter']['hostnames']['buckets']),res['aggregations']['communityfilter']['doc_count']
         except ElasticsearchException as err:
@@ -277,6 +298,8 @@ def setAlertsOverTime(timeframe, numberTpots, numberHoneypots, numberAlerts, cin
     domain=cindex
     # print("timeframe: " + str(span) + "min | number of alerts: " +  str(alert_count)+ " | determined number of T-Pot installations: " + str(tpot_count) + " | number of honeypot daemons: " + str(honeypot_count) + " | community: " + str(cindex))
 
+    # TODO: Store data in DB
+
     return True
 
 
@@ -286,10 +309,11 @@ def setAlertsOverTime(timeframe, numberTpots, numberHoneypots, numberAlerts, cin
 #######################
 
 if __name__ == '__main__':
-    if (len(sys.argv)) != 2 or not sys.argv[1].isdecimal() :
-        print ("Usage: " + sys.argv[0] + " <time_in_minutes>")
-        sys.exit(1)
 
+    parser = argparse.ArgumentParser(description='PEBA Honeypot Statistics.')
+    parser.add_argument('minutes', default=5, nargs='?', type=int, help='number of minutes to consider, default: 5')
+    parser.add_argument('-v',"--verbose",action='store_true',help='verbose: output detailed numbers per honeypot daemon')
+    args=parser.parse_args()
 
     if not testElasticsearch():
         print("ELASTIC SEARCH UNREACHABLE. EXITING")
@@ -300,11 +324,11 @@ if __name__ == '__main__':
 
     #### Retrieve Data
 
-    print("Retrieving statistics for the last " + sys.argv[1] + " minutes:")
+    print("Retrieving statistics for the last " + str(args.minutes)+ " minute(s):")
 
     # DTAG
-    handleHoneypotAlerts(sys.argv[1], False)
+    handleHoneypotAlerts(args.minutes, False)
 
 
     # Community
-    handleHoneypotAlerts(sys.argv[1], True)
+    handleHoneypotAlerts(args.minutes, True)
