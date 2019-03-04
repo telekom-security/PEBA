d52d911b3279df8d0dd3bf873a4b1b8ec13abcef
recrudesce <recrudesce@gmail.com>
2019-03-04 14:22:51 +0000

    Remove extraneous commented lines

--- var/lib/GeoIP/download.sh
+++ var/lib/GeoIP/download.sh
@@ -3,13 +3,6 @@
 
 rm Geo*.gz
 rm Geo*.mmdb
-#wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-#wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-#wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
-#gunzip GeoLiteCity.dat.gz
-#gunzip GeoIP.dat.gz
-#gunzip GeoIPASNum.dat.gz
-
 
 wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
 wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz
