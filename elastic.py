47cb7f2b1fb3136aa47b280f885963234449432c
vorband <andre@vorbach.org>
2019-03-13 18:51:08 +0100

    added support for geoip2
    on running installations need to manually transfer the new databases
    /var/lib/GeoIP/*.mmdb

--- elastic.py
+++ elastic.py
@@ -1,5 +1,6 @@
 from flask import current_app as app
-import pygeoip, datetime
+import geoip2.database
+import datetime
 import hashlib
 import ipaddress
 import base64
@@ -15,10 +16,6 @@ from flask_elasticsearch import FlaskElasticsearch
 ##################
 # PUT ES Variables
 ##################
-
-countries = ["AD","Andorra","AE","United Arab Emirates","AG","Antigua and Barbuda","AI","Anguilla","AL","Albania","AM","Armenia","AO","Angola","AQ","Antarctica","AR","Argentina","AS","American Samoa","AT","Austria","AU","Australia","AW","Aruba","AX","Åland Islands","AZ","Azerbaijan","BA","Bosnia and Herzegovina","BB","Barbados","BD","Bangladesh","BE","Belgium","BF","Burkina Faso","BG","Bulgaria","BH","Bahrain","BI","Burundi","BJ","Benin","BL","Saint Barthélemy","BM","Bermuda","BN","Brunei Darussalam","BO","Bolivia, Plurinational State of","BQ","Bonaire, Sint Eustatius and Saba","BR","Brazil","BS","Bahamas","BT","Bhutan","BV","Bouvet Island","BW","Botswana","BY","Belarus","BZ","Belize","CA","Canada","CC","Cocos (Keeling) Islands","CD","Congo, the Democratic Republic of the","CF","Central African Republic","CG","Congo","CH","Switzerland","CI","Côte d'Ivoire","CK","Cook Islands","CL","Chile","CM","Cameroon","CN","China","CO","Colombia","CR","Costa Rica","CU","Cuba","CV","Cape Verde","CW","Curaçao","CX","Christmas Island","CY","Cyprus","CZ","Czech Republic","DE","Germany","DJ","Djibouti","DK","Denmark","DM","Dominica","DO","Dominican Republic","DZ","Algeria","EC","Ecuador","EE","Estonia","EG","Egypt","EH","Western Sahara","ER","Eritrea","ES","Spain","ET","Ethiopia","FI","Finland","FJ","Fiji","FK","Falkland Islands (Malvinas)","FM","Micronesia, Federated States of","FO","Faroe Islands","FR","France","GA","Gabon","GB","United Kingdom","GD","Grenada","GE","Georgia","GF","French Guiana","GG","Guernsey","GH","Ghana","GI","Gibraltar","GL","Greenland","GM","Gambia","GN","Guinea","GP","Guadeloupe","GQ","Equatorial Guinea","GR","Greece","GS","South Georgia and the South Sandwich Islands","GT","Guatemala","GU","Guam","GW","Guinea-Bissau","GY","Guyana","HK","Hong Kong","HM","Heard Island and McDonald Islands","HN","Honduras","HR","Croatia","HT","Haiti","HU","Hungary","ID","Indonesia","IE","Ireland","IL","Israel","IM","Isle of Man","IN","India","IO","British Indian Ocean Territory","IQ","Iraq","IR","Iran, Islamic Republic of","IS","Iceland","IT","Italy","JE","Jersey","JM","Jamaica","JO","Jordan","JP","Japan","KE","Kenya","KG","Kyrgyzstan","KH","Cambodia","KI","Kiribati","KM","Comoros","KN","Saint Kitts and Nevis","KP","Korea, Democratic People's Republic of","KR","Korea, Republic of","KW","Kuwait","KY","Cayman Islands","KZ","Kazakhstan","LA","Lao People's Democratic Republic","LB","Lebanon","LC","Saint Lucia","LI","Liechtenstein","LK","Sri Lanka","LR","Liberia","LS","Lesotho","LT","Lithuania","LU","Luxembourg","LV","Latvia","LY","Libya","MA","Morocco","MC","Monaco","MD","Moldova, Republic of","ME","Montenegro","MF","Saint Martin (French part)","MG","Madagascar","MH","Marshall Islands","MK","Macedonia, the Former Yugoslav Republic of","ML","Mali","MM","Myanmar","MN","Mongolia","MO","Macao","MP","Northern Mariana Islands","MQ","Martinique","MR","Mauritania","MS","Montserrat","MT","Malta","MU","Mauritius","MV","Maldives","MW","Malawi","MX","Mexico","MY","Malaysia","MZ","Mozambique","NA","Namibia","NC","New Caledonia","NE","Niger","NF","Norfolk Island","NG","Nigeria","NI","Nicaragua","NL","Netherlands","NO","Norway","NP","Nepal","NR","Nauru","NU","Niue","NZ","New Zealand","OM","Oman","PA","Panama","PE","Peru","PF","French Polynesia","PG","Papua New Guinea","PH","Philippines","PK","Pakistan","PL","Poland","PM","Saint Pierre and Miquelon","PN","Pitcairn","PR","Puerto Rico","PS","Palestine, State of","PT","Portugal","PW","Palau","PY","Paraguay","QA","Qatar","RE","Réunion","RO","Romania","RS","Serbia","RU","Russian Federation","RW","Rwanda","SA","Saudi Arabia","SB","Solomon Islands","SC","Seychelles","SD","Sudan","SE","Sweden","SG","Singapore","SH","Saint Helena, Ascension and Tristan da Cunha","SI","Slovenia","SJ","Svalbard and Jan Mayen","SK","Slovakia","SL","Sierra Leone","SM","San Marino","SN","Senegal","SO","Somalia","SR","Suriname","SS","South Sudan","ST","Sao Tome and Principe","SV","El Salvador","SX","Sint Maarten (Dutch part)","SY","Syrian Arab Republic","SZ","Swaziland","TC","Turks and Caicos Islands","TD","Chad","TF","French Southern Territories","TG","Togo","TH","Thailand","TJ","Tajikistan","TK","Tokelau","TL","Timor-Leste","TM","Turkmenistan","TN","Tunisia","TO","Tonga","TR","Turkey","TT","Trinidad and Tobago","TV","Tuvalu","TW","Taiwan, Province of China","TZ","Tanzania, United Republic of","UA","Ukraine","UG","Uganda","UM","United States Minor Outlying Islands","US","United States","UY","Uruguay","UZ","Uzbekistan","VA","Vatican City State","VC","Saint Vincent and the Grenadines","VE","Venezuela, Bolivarian Republic of","VG","Virgin Islands, British","VI","Virgin Islands, U.S.","VN","Viet Nam","VU","Vanuatu","WF","Wallis and Futuna","WS","Samoa","YE","Yemen","YT","Mayotte","ZA","South Africa","ZM","Zambia","ZW","Zimbabwe",
-            "", ""]
-
 
 
 ##################
@@ -41,62 +38,42 @@ def setCache(cacheItem, cacheValue, cacheTimeout, cache, cacheType):
   except:
         app.logger.error("Could not set memcache cache {0} to value {1} and Timeout {2}".format(cacheTypeItem, str(cacheValue), cacheTimeout))
 
-
-def getCountries(id):
-    """return the country name for country code"""
-    for i in range (0,len(countries) - 2, 2):
-         shortCode = countries[i]
-         countryName = countries[i+1]
-
-         if (shortCode in id):
-             return countryName
-
-    return ""
-
-
-
 def getGeoIPNative(sourceip, cache):
 
     """ get geoip and ASN information from IP """
 
-    gi = pygeoip.GeoIP("/var/lib/GeoIP/GeoIP.dat")
-    giCity = pygeoip.GeoIP("/var/lib/GeoIP/GeoLiteCity.dat")
-    giASN = pygeoip.GeoIP('/var/lib/GeoIP/GeoIPASNum.dat')
+    gi = geoip2.database.Reader("/var/lib/GeoIP/GeoLite2-Country.mmdb")
+    giCity = geoip2.database.Reader("/var/lib/GeoIP/GeoLite2-City.mmdb")
+    giASN = geoip2.database.Reader('/var/lib/GeoIP/GeoLite2-ASN.mmdb')
 
-    ASN_fail = "-"
-    country_fail = "-"
-    ASN_fail_text = "-"
+
+    lat = "0.0"
+    long = "0.0"
+    country = "-"
+    countryName = "-"
+    asn = "-"
 
     try:
+        # local ips
         if ipaddress.ip_address(sourceip).is_private:
-            ASN_fail="IANA"
-            ASN_fail_text="IANA Private IP Range"
-            country_fail = "PIR"
+            asn="IANA"
+            countryName="IANA Private IP Range"
+            country = "PIR"
 
-        asn = giASN.org_by_addr(sourceip)
-        if (asn == "" ) or asn is None:
-            setCache(sourceip, "0.0" + "|" + "0.0" + "|" + country_fail + "|" + ASN_fail + "|" + ASN_fail_text, 60 * 60 * 24, cache, "ip")
-            return ("0.0", "0.0", "-", "-", "-")
-
-        country = gi.country_code_by_addr(sourceip)
-        if (country == "") or country is None:
-            setCache(sourceip, "0.0" + "|" + "0.0" + "|" + country_fail + "|" + ASN_fail + "|" + ASN_fail_text, 60 * 60 * 24, cache, "ip")
-            return ("0.0", "0.0", "-", "-", "-")
-
-        long = giCity.record_by_addr(sourceip)['longitude']
-        lat = giCity.record_by_addr(sourceip)['latitude']
-        countryName = getCountries(country)
-        asn = giASN.org_by_addr(sourceip)
-
-        # store data in memcache
-        setCache(sourceip, str(lat) + "|" + str(long) + "|" + country + "|"+ asn  + "|" + countryName, 60*60*24, cache, "ip")
-
-        return (lat, long, country, asn, countryName)
-
+        # resolve IPs
+        else:
+            asn = "AS"+ str(giASN.asn(sourceip).autonomous_system_number) + " " + giASN.asn(sourceip).autonomous_system_organization
+            long = str(giCity.city(sourceip).location.longitude)
+            lat = str(giCity.city(sourceip).location.latitude)
+            if gi.country(sourceip).country.name != None:
+                countryName = gi.country(sourceip).country.name
+            if gi.country(sourceip).country.iso_code != None:
+                country = gi.country(sourceip).country.iso_code
     except:
-        setCache(sourceip, "0.0" + "|" + "0.0" + "|" + country_fail + "|" + ASN_fail + "|" + ASN_fail_text, 60 * 60 * 24, cache, "ip")
-        return ("0.0", "0.0", country_fail, ASN_fail, ASN_fail_text)
+        pass
 
+    setCache(sourceip, lat + "|" + long + "|" + country + "|"+ asn  + "|" + countryName, 60*60*24, cache, "ip")
+    return (lat, long, country, asn, countryName)
 
 
 
